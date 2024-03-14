9ee1e0c01c9414ea7ef5e553e74f0a88ecd8805c
GeroKramar <gerokramar@gmail.com>
2024-03-14 16:12:40 -0300

    homework

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,93 +1,27 @@
-// creacion de entidad card 
+
+const renderCard = require('./renderCard')
+const axios = require('axios')
 
 
-class Card{
-    constructor(id ,title, year, director, duration, poster){
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.director = director;
-        this.duration = duration;
-        this.poster = poster;
 
+
+const fetchMoviesCards = async() => {
+    try {
+      const data = await axios.get('https://students-api.2.us-1.fl0.io/movies')
+        renderCard(data); 
+        
+    } catch (error) {
+        console.log('No se pudieron cargar las cards')
+        console.log(error);
     }
-}
 
-//creacion de metodos para card
-
-class Repository {
-    constructor(){
-        this.cards = [];
-        this.id = 0;
-    }
-    getAllCards(){
-        return this.cards;
-    }
-    createCard({title, year, director, duration, poster}){
-        this.id++;
-        const card = new Card(this.id, title, year, director, duration, poster);
-        this.cards.push(card);
-        return card; // Devolver la tarjeta creada
-    }
-}
-// Instacia de metodos 
-const repository = new Repository();
-
-//mapeo de funcion
-const mapCards = card => {
-    const cardElement = document.createElement('div');
-    cardElement.className = 'card';
-
-    const img = document.createElement('img');
-    img.src = card.poster; // Asegúrate de que 'card.poster' es la URL de la imagen
-    img.alt = `Poster de ${card.title}`;
-
-    const descriptions = document.createElement('div');
-    descriptions.className = 'descriptions';
-
-    const h1 = document.createElement('h1');
-    h1.textContent = card.title;
-
-    const p1 = document.createElement('p');
-    p1.textContent = `Year: ${card.year}` 
-    const p2 = document.createElement('p');
-    p2.textContent = `Director: ${card.director}` 
-    const p3 = document.createElement('p');
-    p3.textContent = `Duration: ${card.duration}`;
-    
-
-    const button = document.createElement('button');
-    button.textContent = 'Ver Ahora';
-
-    descriptions.appendChild(h1);
-    descriptions.appendChild(p1);
-    descriptions.appendChild(p2);
-    descriptions.appendChild(p3);
-    descriptions.appendChild(button);
-
-    cardElement.appendChild(img);
-    cardElement.appendChild(descriptions);
-
-    return cardElement;
-};
-// Creacion de tarjetas en el DOM
-const renderCard = (movies) => {
-    const contentCards = document.getElementById('content-cards');
-    contentCards.innerHTML = ''; // Limpiar el contenido existente
-
-    movies.forEach(movie => {
-        // Crear una tarjeta para cada película
-        const card = repository.createCard(movie); // Asumiendo que `movie` tiene las propiedades necesarias
-        const cardElement = mapCards(card); // Crear el elemento DOM para la tarjeta
-        contentCards.appendChild(cardElement); // Agregar la tarjeta al DOM
-    });
 }
 
 
-// Consumo de end point con Jquery
-$.get('https://students-api.2.us-1.fl0.io/movies', function(response) {
-    renderCard(response); // Asegúrate de que `response` es el arreglo de películas
-});
+
+//  Consumo de end point con Jquery
+// $.get('https://students-api.2.us-1.fl0.io/movies', function(response) {
+// });
 
 
 
---
+++ front/scripts/renderCard.js
@@ -0,0 +1,87 @@
+
+// creacion de entidad card 
+
+
+class Card{
+    constructor(id ,title, year, director, duration, poster){
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.duration = duration;
+        this.poster = poster;
+
+    }
+}
+
+//creacion de metodos para card
+
+class Repository {
+    constructor(){
+        this.cards = [];
+        this.id = 0;
+    }
+    getAllCards(){
+        return this.cards;
+    }
+    createCard({title, year, director, duration, poster}){
+        this.id++;
+        const card = new Card(this.id, title, year, director, duration, poster);
+        this.cards.push(card);
+        return card; // Devolver la tarjeta creada
+    }
+}
+// Instacia de metodos 
+const repository = new Repository();
+
+//mapeo de funcion
+const mapCards = card => {
+    const cardElement = document.createElement('div');
+    cardElement.className = 'card';
+
+    const img = document.createElement('img');
+    img.src = card.poster; // Asegúrate de que 'card.poster' es la URL de la imagen
+    img.alt = `Poster de ${card.title}`;
+
+    const descriptions = document.createElement('div');
+    descriptions.className = 'descriptions';
+
+    const h1 = document.createElement('h1');
+    h1.textContent = card.title;
+
+    const p1 = document.createElement('p');
+    p1.textContent = `Year: ${card.year}` 
+    const p2 = document.createElement('p');
+    p2.textContent = `Director: ${card.director}` 
+    const p3 = document.createElement('p');
+    p3.textContent = `Duration: ${card.duration}`;
+    
+
+    const button = document.createElement('button');
+    button.textContent = 'Ver Ahora';
+
+    descriptions.appendChild(h1);
+    descriptions.appendChild(p1);
+    descriptions.appendChild(p2);
+    descriptions.appendChild(p3);
+    descriptions.appendChild(button);
+
+    cardElement.appendChild(img);
+    cardElement.appendChild(descriptions);
+
+    return cardElement;
+};
+// Creacion de tarjetas en el DOM
+const renderCard = (movies) => {
+    const contentCards = document.getElementById('content-cards');
+    contentCards.innerHTML = ''; // Limpiar el contenido existente
+
+    movies.forEach(movie => {
+        // Crear una tarjeta para cada película
+        const card = repository.createCard(movie); // Asumiendo que `movie` tiene las propiedades necesarias
+        const cardElement = mapCards(card); // Crear el elemento DOM para la tarjeta
+        contentCards.appendChild(cardElement); // Agregar la tarjeta al DOM
+    });
+}
+
+module.exports = renderCard;
